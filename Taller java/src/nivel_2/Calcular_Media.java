10fbf304b8da66681e853d0f7e498b74efeaaa8a
mmerchan211 <mmerchan889@unab.edu.co>
2025-10-29 11:29:52 -0500

    Update Calcular_Media.java

--- Taller java/src/nivel_2/Calcular_Media.java
+++ Taller java/src/nivel_2/Calcular_Media.java
@@ -1,18 +1,23 @@
 package nivel_2;
-
+import java.util.Scanner;
 public class Calcular_Media {
-    public static void calcular_media(double[] lista) {
-        if (lista == null || lista.length == 0) {
-            System.out.println("La lista está vacía.");
-            return;
-        }
+public static void calcularMedia() {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Ingrese varios números separados por comas: ");
+        String entrada = sc.nextLine();
+
+        String[] partes = entrada.split(",");
         double suma = 0;
-        for (double v : lista) suma += v;
-        double media = suma / lista.length;
-        System.out.println("La media es: " + media);
+        for (String num : partes) {
+            suma += Double.parseDouble(num.trim());
+        }
+
+        double media = suma / partes.length;
+        System.out.println("La media de los números es: " + media);
     }
 
     public static void main(String[] args) {
-        calcular_media(new double[] {2,18,36,40});
+        calcularMedia();
     }
 }
