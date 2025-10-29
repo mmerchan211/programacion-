1266be4f01dce3dc93df2415659fe4cffb944f9b
mmerchan211 <mmerchan889@unab.edu.co>
2025-10-29 11:28:52 -0500

    Update Calcular_Potencia.java

--- Taller java/src/nivel_2/Calcular_Potencia.java
+++ Taller java/src/nivel_2/Calcular_Potencia.java
@@ -1,12 +1,20 @@
 package nivel_2;
-
+import java.util.Scanner;
 public class Calcular_Potencia {
-    public static void calcular_potencia(double base, double exponente) {
+     public static void calcularPotencia() {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Ingrese la base: ");
+        double base = sc.nextDouble();
+
+        System.out.print("Ingrese el exponente: ");
+        double exponente = sc.nextDouble();
+
         double resultado = Math.pow(base, exponente);
-        System.out.println(base + " elevado a " + exponente + " es " + resultado);
+        System.out.println("El resultado de " + base + " elevado a " + exponente + " es: " + resultado);
     }
 
     public static void main(String[] args) {
-        calcular_potencia(6,8);
+        calcularPotencia();
     }
 }
