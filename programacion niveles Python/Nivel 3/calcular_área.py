57c1ec87b38c8a01d107d1a93b61cef9f44fce50
mmerchan211 <mmerchan889@unab.edu.co>
2025-10-29 10:27:02 -0500

    Update calcular_área.py

--- programacion niveles Python/Nivel 3/calcular_área.py
+++ programacion niveles Python/Nivel 3/calcular_área.py
@@ -3,8 +3,9 @@ import math
 radio = float(input("Ingresa el radio del cilindro: "))
 altura = float(input("Ingresa la altura del cilindro: "))
 
-# Calcular el área
+# área
 area = (2 * math.pi * radio**2) + (2 * math.pi * radio * altura)
 
-# Mostrar el resultado
+#resultado
+
 print(f"El área del cilindro es: {area:.2f}")
