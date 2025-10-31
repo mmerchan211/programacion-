707bea7ad7e297055cc559b5f48a42f08f6aec9b
mmerchan211 <mmerchan889@unab.edu.co>
2025-10-30 21:46:49 -0500

    Update Mayor_de_tres_numeros.java

--- Taller java/src/nivel_3/Mayor_de_tres_numeros.java
+++ Taller java/src/nivel_3/Mayor_de_tres_numeros.java
@@ -1,10 +1,23 @@
 package nivel_3;
+import java.util.Scanner;
+
 public class Mayor_de_tres_numeros {
-    public static double mayor_de_tres_numeros(double a, double b, double c) {
-        return Math.max(a, Math.max(b, c));
+    public static double mayorDeTresNumeros(double n1, double n2, double n3) {
+        return Math.max(n1, Math.max(n2, n3));
     }
+
     public static void main(String[] args) {
-        System.out.println(mayor_de_tres_numeros(78,48,102));
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Ingresa el primer número: ");
+        double a = sc.nextDouble();
+        System.out.print("Ingresa el segundo número: ");
+        double b = sc.nextDouble();
+        System.out.print("Ingresa el tercer número: ");
+        double c = sc.nextDouble();
+
+        double mayor = mayorDeTresNumeros(a, b, c);
+        System.out.println("El número mayor es: " + mayor);
+        sc.close();
     }
 }
 
