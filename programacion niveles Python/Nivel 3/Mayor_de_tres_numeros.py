83b920cefbf77706e3dd9da91b0fdf59ee5633d7
mmerchan211 <mmerchan889@unab.edu.co>
2025-10-30 22:01:48 -0500

    Update Mayor_de_tres_numeros.py

--- programacion niveles Python/Nivel 3/Mayor_de_tres_numeros.py
+++ programacion niveles Python/Nivel 3/Mayor_de_tres_numeros.py
@@ -1,4 +1,10 @@
-def mayor_de_tres_numeros(a,b,c):
-    return max(a,b,c)
+def mayor_de_tres_numeros(numero1: float, numero2: float, numero3: float) -> float:
+    return max(numero1, numero2, numero3)
 
-print(mayor_de_tres_numeros(15,85,150))  
+# main
+if __name__ == "__main__":
+    a = float(input("Ingrese el primer número: "))
+    b = float(input("Ingrese el segundo número: "))
+    c = float(input("Ingrese el tercer número: "))
+    mayor = mayor_de_tres_numeros(a, b, c)
+    print("El número mayor es:", mayor)
